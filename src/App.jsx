6b8de5bfca2a5578a86b04b3d7d126045f3c1513
sAndrin3 <sandrinemalanga2001@gmail.com>
2025-09-09 15:25:39 +0300

    Pagination, Column ordering and Sticky table

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import {BasicTable} from "./components/BasicTable.jsx";
 import {SortingTable} from "./components/SortingTable.jsx";
 import {FilteringTable} from "./components/FilteringTable.jsx";
+import {PaginationTable} from "./components/PaginationTable.jsx";
+import {RowSelection} from "./components/RowSelection.jsx";
+import {ColumnOrder} from "./components/ColumnOrder.jsx";
+import {ColumnHiding} from "./components/ColumnHiding.jsx";
+import {StickyTable} from "./components/StickyTable.jsx";
 
 const App = () => {
     return (
-        <div><FilteringTable/></div>
+        <div><StickyTable/></div>
     )
 }
 
--- src/components/columns.jsx
+++ src/components/columns.jsx
@@ -5,17 +5,20 @@ export const COLUMNS = [
         Header: 'Id',
         Footer: 'Id',
         accessor: 'id',
-        disableFilters: true
+        disableFilters: true,
+        sticky: 'left'
     },
     {
         Header: 'First Name',
         Footer: 'First Name',
-        accessor: 'first_name'
+        accessor: 'first_name',
+        sticky: 'left'
     },
     {
         Header: 'Last Name',
         Footer: 'Last Name',
-        accessor: 'last_name'
+        accessor: 'last_name',
+        sticky: 'left'
     },
     {
         Header: 'Date Of Birth',
@@ -32,6 +35,16 @@ export const COLUMNS = [
         Header: 'Phone',
         Footer: 'Phone',
         accessor: 'phone'
+    },
+    {
+        Header: 'Email',
+        Footer: 'Email',
+        accessor: 'email'
+    },
+    {
+        Header: 'Age',
+        Footer: 'Age',
+        accessor: 'age'
     }
 ]
 
